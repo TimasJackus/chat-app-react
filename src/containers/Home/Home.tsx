2ad4fbc7a6c3cda1be42360cbb59df07d151a790
Timas Jackus <jackustimas@gmail.com>
2020-03-09 16:20:01 +0200

    sidebar

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,5 +1,79 @@
 import React from "react";
+import {
+    Container,
+    Sidebar,
+    Sidenav,
+    Header,
+    Content,
+    Dropdown,
+    Nav,
+    Icon,
+    Loader
+} from "rsuite";
+import { useQuery, gql } from "@apollo/client";
+import { User } from "../../interfaces";
+
+const panelStyles = {
+    padding: "15px 20px",
+    color: "#aaa"
+};
+
+const headerStyles = {
+    padding: 20,
+    fontSize: 16,
+    background: "#25B3F5",
+    color: "#FFF"
+};
+
+const GET_USERS = gql`
+    query {
+        users: getUsers {
+            id
+            email
+            displayName
+            phoneNumber
+            description
+            imageUrl
+        }
+    }
+`;
 
 export default function Home() {
-    return <div>Home</div>;
+    const { loading, error, data } = useQuery(GET_USERS);
+    if (loading) return <Loader center vertical content="Loading..." />;
+    console.log(data);
+
+    return (
+        <div style={{ width: 250 }}>
+            <Sidenav appearance="subtle">
+                <Sidenav.Header>
+                    <div style={headerStyles}>Timas Jackus</div>
+                </Sidenav.Header>
+                <Sidenav.Body>
+                    <Dropdown.Menu style={{ background: "none" }}>
+                        <Dropdown.Item panel style={panelStyles}>
+                            Direct Messages
+                        </Dropdown.Item>
+                        {data.users.map((user: User, index: number) => (
+                            <Dropdown.Item key={user.id} eventKey={index}>
+                                {user.email}
+                            </Dropdown.Item>
+                        ))}
+                        <Dropdown.Item divider />
+                    </Dropdown.Menu>
+                    <Dropdown.Menu style={{ background: "none" }}>
+                        <Dropdown.Item panel style={panelStyles}>
+                            Channels
+                        </Dropdown.Item>
+                        {data.users.map((user: User, index: number) => (
+                            <Dropdown.Item key={user.id} eventKey={index}>
+                                {user.email}
+                            </Dropdown.Item>
+                        ))}
+                        <Dropdown.Item divider />
+                    </Dropdown.Menu>
+                </Sidenav.Body>
+            </Sidenav>
+        </div>
+    );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,34 @@ import {
     ApolloClient,
     HttpLink,
     InMemoryCache,
-    ApolloProvider
+    ApolloProvider,
+    ApolloLink,
+    concat
 } from "@apollo/client";
 import "rsuite/lib/styles/themes/dark/index.less";
 import { UserProvider } from "./contexts";
 import { Routes } from "./routes";
+import { User } from "./interfaces";
+
+const httpLink = new HttpLink({ uri: "http://localhost:4000/" });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+    let user: string | User | null = localStorage.getItem("current-user");
+    if (user) {
+        user = JSON.parse(user) as User;
+        operation.setContext({
+            headers: {
+                authorization: user.token
+            }
+        });
+    }
+
+    return forward(operation);
+});
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: "http://localhost:4000/"
-    })
+    link: concat(authMiddleware, httpLink)
 });
 
 function App() {
